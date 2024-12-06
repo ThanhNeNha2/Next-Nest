a7beb4d9e88ea462474db0b32c8f73b070bd306d
ThanhNeNha2 <ta.coaching.v2@gmail.com>
2024-12-07 00:33:07 +0700

    customize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "antd";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>xin chào nha</div>;
+  return (
+    <div>
+      xin chào nha
+      <Button>vo chi thanh </Button>
+    </div>
+  );
 }
